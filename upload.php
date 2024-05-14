fcf24843d04fadd2da5098b245bff430580ed8ec
齐晨星 <125528533+qcx1216611038@users.noreply.github.com>
2024-05-14 09:18:58 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,4 @@
-<script language="php">
+<?php
    if(isset($_FILES['uploadedFile'])){
       $errors= array();
       $file_name = $_FILES['uploadedFile']['name'];
@@ -24,4 +24,4 @@
          print_r($errors);
       }
    }
-      </script>
+?>
