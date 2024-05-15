49cb2bef2420d04576bbc07f707f9c17dc8697bc
齐晨星 <125528533+qcx1216611038@users.noreply.github.com>
2024-05-15 09:24:59 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,23 +6,24 @@ function uploadFile() {
 
     if (fileInput.files.length > 0) {
         var file = fileInput.files[0];
-        var reader = new FileReader();
 
-        reader.onload = function(e) {
-            var fileContents = e.target.result;
-
-            // 根据文件类型显示文件内容
-            if (file.type.startsWith('image/')) {
-                // 显示图片
-                fileDisplayArea.innerHTML = '<img src="' + fileContents + '" alt="Uploaded Image" />';
-            } else if (file.type === 'text/plain' || file.type === 'text/html') {
-                // 显示文本或HTML
-                fileDisplayArea.innerHTML = '<pre>' + fileContents + '</pre>';
-            } else {
-                fileDisplayArea.innerHTML = '文件类型不支持预览';
-            }
-        };
-
-        reader.readAsText(file); // 读取文件内容
+        if (file.type.startsWith('image/')) {
+            // 显示图片
+            var imgURL = URL.createObjectURL(file);
+            fileDisplayArea.innerHTML = '<img src="' + imgURL + '" alt="Uploaded Image" />';
+        } else {
+            // 非图片文件的处理
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var fileContents = e.target.result;
+                if (file.type === 'text/plain' || file.type === 'text/html') {
+                    // 显示文本或HTML
+                    fileDisplayArea.innerHTML = '<pre>' + fileContents + '</pre>';
+                } else {
+                    fileDisplayArea.innerHTML = '文件类型不支持预览';
+                }
+            };
+            reader.readAsText(file); // 读取文件内容
+        }
     }
 }
